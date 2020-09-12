9826e2ffd2aaccabcedea8870631ab0c0ac01827
ivjot singh <ivjotsingh@ivjots-MacBook-Pro.local>
2020-09-12 23:50:58 +0530

    route added

--- app.js
+++ app.js
@@ -4,8 +4,25 @@ const http = require('http');
 
  const PORT = process.env.PORT || 8000
 
+
+ const joinRoute = require('./routes/join');
+const router = require('./routes/join');
+
+
  const app = express();
  const server = http.createServer(app);
  const io = socketio(server)
+
+
+
+ io.on('connection', (socket) => {
+    console.log("someone joined the socket")
+
+    socket.on('disconnect', () => {
+        console.log("socket connection broke")
+    })
+ })
+
+ app.use(router);
 
  server.listen(PORT, () => console.log(`Server listening on Port ${PORT} ...`))
