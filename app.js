6b441485bbbf5450e7286152ccff173710920075
ivjot singh <ivjotsingh@ivjots-MacBook-Pro.local>
2020-09-12 23:56:01 +0530

    Join route added

--- app.js
+++ app.js
@@ -2,27 +2,24 @@ const express = require('express');
 const socketio  = require('socket.io');
 const http = require('http');
 
- const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 8000
+
+const joinRoute = require('./routes/join');
 
 
- const joinRoute = require('./routes/join');
-const router = require('./routes/join');
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server)
 
 
- const app = express();
- const server = http.createServer(app);
- const io = socketio(server)
+io.on('connection', (socket) => {
+console.log("someone joined the socket")
+
+socket.on('disconnect', () => {
+    console.log("socket connection broke")
+})
+})
 
 
-
- io.on('connection', (socket) => {
-    console.log("someone joined the socket")
-
-    socket.on('disconnect', () => {
-        console.log("socket connection broke")
-    })
- })
-
- app.use(router);
-
- server.listen(PORT, () => console.log(`Server listening on Port ${PORT} ...`))
+app.use('/join', joinRoute)
+server.listen(PORT, () => console.log(`Server listening on Port ${PORT} ...`))
