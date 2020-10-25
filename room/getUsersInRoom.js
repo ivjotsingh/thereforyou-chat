ab98effa582f9cde916205d5bfb05974d574aee7
dimple verma <dimple97verma@gmail.com>
2020-10-26 00:22:07 +0530

    Modified getUsersInRoom service to return userId instead of user object

--- room/getUsersInRoom.js
+++ room/getUsersInRoom.js
@@ -3,7 +3,11 @@ const Room = require("../room/room")
 exports.getUsersInRoom = async (room)=>{
     try{
     let sessionUsers = await Room.find({_id =room._id});
-    return {users:sessionUsers};
+    let sessionUsersId =[];
+    for (let sessionuser in sessionUsers){
+        sessionUsersId.push(sessionuser.user);
+    }
+    return {users:sessionUsersId};
     }
     catch(err){
         return {error:err};
